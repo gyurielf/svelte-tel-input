cbc1420f0b0375651988fca139a5a056f9465fa5
gyurielf <mail.gyorgy.kallai@gmail.com>
2022-07-20 21:19:52 +0200

    fix: remove spa mode

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event, {
-		ssr: false
-	});
-
-	return response;
+	return await resolve(event);
 };
