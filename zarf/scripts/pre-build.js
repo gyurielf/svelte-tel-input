24c702f51eb3134bc9600a14ccb933aa45453429
gyurielf <mail.gyorgy.kallai@gmail.com>
2023-01-07 13:04:48 +0100

    fix: remove types field before packaging

--- zarf/scripts/pre-build.js
+++ zarf/scripts/pre-build.js
@@ -7,11 +7,3 @@ import { readFileSync, writeFileSync } from 'fs';
 writeFileSync('package.json', del(readFileSync('package.json').toString(), 'exports'));
 // Remove types field of ../package.json
 writeFileSync('package.json', del(readFileSync('package.json').toString(), 'types'));
-
-//Put the appropriate types field into package.json.
-let packageJson = readFileSync('package.json').toString();
-packageJson = packageJson.slice(0, packageJson.lastIndexOf('}') - 1); //strip closing }
-packageJson += `,
-    "types": "./types/index.d.ts"
-}`;
-writeFileSync('package.json', packageJson);
