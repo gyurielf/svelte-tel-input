0b9cafff6f672470584d33528e2ec88c59a78f9e
gyurielf <mail.gyorgy.kallai@gmail.com>
2023-01-17 17:45:22 +0100

    fix: remove duplicated directive file

--- src/lib/components/utils/seoJsonLdAction.ts
+++
@@ -1,8 +0,0 @@
-import type { Thing, WithContext } from 'schema-dts';
-
-export const jsonLdScript = (node: HTMLScriptElement, jsonLd: (Thing | WithContext<Thing>)[]) => {
-	if (node) {
-		node.type = 'application/ld+json';
-		node.text = JSON.stringify(jsonLd, null, 2);
-	}
-};
