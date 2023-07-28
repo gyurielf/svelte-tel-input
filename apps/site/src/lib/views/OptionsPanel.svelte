063d6a78fe1a9d7c582fb0982ae69396ee086161
gyurielf <mail.gyorgy.kallai@gmail.com>
2023-07-28 08:58:07 +0200

    fix: markup ids

--- apps/site/src/lib/views/OptionsPanel.svelte
+++ apps/site/src/lib/views/OptionsPanel.svelte
@@ -1,12 +1,18 @@
+<script context="module">
+	let counter = 0;
+</script>
+
 <script lang="ts">
 	import type { TelInputOptions } from 'svelte-tel-input/types';
 	import { slide } from 'svelte/transition';
 	export let options: TelInputOptions;
 	let isOpen = false;
+	let markupId = counter++;
 </script>
 
 <div class="validation-table mt-5 rounded-t">
 	<button
+		type="button"
 		class="w-full text-left dark:bg-gray-700 dark:hover:bg-gray-600 transition bg-gray-100 hover:bg-gray-200 p-3 rounded-t cursor-pointer"
 		class:rounded-b={!isOpen}
 		on:click={() => {
@@ -58,7 +64,7 @@
 				class="relative inline-flex items-center align-middle cursor-pointer"
 			>
 				<input
-					id="spaces"
+					id="spaces-{markupId}"
 					type="checkbox"
 					value={options.spaces}
 					class="sr-only peer"
@@ -78,7 +84,7 @@
 				class="relative inline-flex items-center align-middle cursor-pointer"
 			>
 				<input
-					id="autoPlaceholder"
+					id="autoPlaceholder-{markupId}"
 					type="checkbox"
 					value={options.autoPlaceholder}
 					class="sr-only peer"
@@ -99,7 +105,7 @@
 				class="relative inline-flex items-center align-middle cursor-pointer"
 			>
 				<input
-					id="invalidateOnCountryChange"
+					id="invalidateOnCountryChange-{markupId}"
 					type="checkbox"
 					value={options.invalidateOnCountryChange}
 					class="sr-only peer"
@@ -117,7 +123,7 @@
 			</label>
 			<div class="inline-flex">
 				<select
-					id="countries"
+					id="formats-{markupId}"
 					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					bind:value={options.format}
 				>
