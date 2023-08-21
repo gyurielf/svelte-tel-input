55dd724e72845d9cd81befbb2f4070adcd45f224
gyurielf <mail.gyorgy.kallai@gmail.com>
2023-08-21 18:37:43 +0200

    fix: update deps

--- packages/svelte-tel-input/scripts/convertExamples.js
+++ packages/svelte-tel-input/scripts/convertExamples.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 const sourceFilePath = 'node_modules/libphonenumber-js/examples.mobile.json.js'; // Direct path to the file in node_modules
 const targetFilePath = './src/lib/assets/examplePhoneNumbers.ts'; // Change to desired path
 const content = fs.readFileSync(sourceFilePath, 'utf-8');
-
-const asd = `import type { Examples } from 'libphonenumber-js'; \n ${content} as const satisfies Examples;`;
-fs.writeFileSync(targetFilePath, asd, 'utf-8');
+const extendedContent = `import type { Examples } from 'libphonenumber-js'; \n${content} as const satisfies Examples;`;
+fs.writeFileSync(targetFilePath, extendedContent, 'utf-8');
 console.log(`Duplicated ${sourceFilePath} to ${targetFilePath}`);
