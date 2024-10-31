4aa0ce0cb4d6eb08dc2ef01fce953d31b897aa58
gyurielf <mail.gyorgy.kallai@gmail.com>
2024-10-31 08:35:54 +0100

    chore: update deps

--- apps/site/src/lib/views/OptionsPanel.svelte
+++ apps/site/src/lib/views/OptionsPanel.svelte
@@ -125,16 +125,6 @@
 					>Invalidate on (manual) country change.</span
 				>
 			</label>
-			<div class="inline-flex">
-				<select
-					id="formats-{markupId}"
-					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					bind:value={options.format}
-				>
-					<option value="national">National</option>
-					<option value="international">International</option>
-				</select>
-			</div>
 		</div>
 	{/if}
 </div>
--- packages/svelte-tel-input/src/tests/inputParserTest.spec.ts
+++ packages/svelte-tel-input/src/tests/inputParserTest.spec.ts
@@ -1,12 +1,11 @@
-import { inputParser, inspectAllowedChars } from '$lib/utils/index.js';
+import { inputParser } from '$lib/utils/index.js';
 import { describe, it, expect } from 'vitest';
 
 describe('Hello.svelte', () => {
 	it('Input should be eleminate leters and special chars, and resul should be trimmed.', () => {
 		const testValue = '+36 30 1aS 34 .9 ';
 		const resut = inputParser(testValue, {
-			allowSpaces: true,
-			parseCharacter: inspectAllowedChars
+			allowSpaces: true
 		});
 		expect(resut).toStrictEqual('+36 30 1 34 9 ');
 	});
@@ -14,8 +13,7 @@ describe('Hello.svelte', () => {
 	it('Input should be eleminate leters and special chars, and resul should be untrimmed.', () => {
 		const testValue = '+36 30 1aS 34 .9 ';
 		const resut = inputParser(testValue, {
-			allowSpaces: true,
-			parseCharacter: inspectAllowedChars
+			allowSpaces: true
 		});
 		expect(resut).toStrictEqual('+36 30 1 34 9 ');
 	});
@@ -23,8 +21,7 @@ describe('Hello.svelte', () => {
 	it('Trimmed input with spaces', () => {
 		const testValue = '+36 30 1a3 45 67';
 		const resut = inputParser(testValue, {
-			allowSpaces: true,
-			parseCharacter: inspectAllowedChars
+			allowSpaces: true
 		});
 		expect(resut).toStrictEqual('+36 30 13 45 67');
 	});
@@ -32,8 +29,7 @@ describe('Hello.svelte', () => {
 	it('Perfect input without spaces', () => {
 		const testValue = '+36a301234567';
 		const resut = inputParser(testValue, {
-			allowSpaces: false,
-			parseCharacter: inspectAllowedChars
+			allowSpaces: false
 		});
 		expect(resut).toStrictEqual('+36301234567');
 	});
